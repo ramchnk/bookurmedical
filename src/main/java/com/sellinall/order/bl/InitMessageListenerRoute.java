939f8cce004a6490210c1033be91316f63684d61
Mallikarjun <malli@sellinall.com>
2015-11-05 11:40:05 +0800

    check for notification type

--- src/main/java/com/sellinall/order/bl/InitMessageListenerRoute.java
+++ src/main/java/com/sellinall/order/bl/InitMessageListenerRoute.java
@@ -6,10 +6,14 @@ import org.apache.log4j.Logger;
 import org.codehaus.jettison.json.JSONObject;
 
 public class InitMessageListenerRoute implements Processor {
+	private static final String ORDER = "order";
 	static Logger log = Logger.getLogger(InitMessageListenerRoute.class.getName());
 	public void process(Exchange exchange) throws Exception {
 		JSONObject message = new JSONObject(exchange.getIn().getBody(String.class));
-		String messageType = message.getString("type");
+		String messageType = ORDER;
+		if ( message.has("type")) {
+			messageType = message.getString("type");
+		}
 		exchange.setProperty("messageType", messageType);
 		exchange.setProperty("message", message);
 		exchange.getOut().setBody(message);

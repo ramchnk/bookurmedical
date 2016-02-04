0738b20ce4a45f56ab26204abbfdc3fd101fb074
Raju Vasanthakumar <raju@sellinall.com>
2016-02-04 15:52:14 +0800

    alignment fix

--- src/main/java/com/sellinall/order/message/PrepareOrderEventPublishMessage.java
+++ src/main/java/com/sellinall/order/message/PrepareOrderEventPublishMessage.java
@@ -2,6 +2,7 @@ package com.sellinall.order.message;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.log4j.Logger;
 import org.codehaus.jettison.json.JSONObject;
 
 /**
@@ -9,11 +10,15 @@ import org.codehaus.jettison.json.JSONObject;
  * @author Raju
  *
  */
-public class PrepareOrderEventPublishMessage implements Processor {
 
+public class PrepareOrderEventPublishMessage implements Processor {
+	static Logger log = Logger.getLogger(PrepareOrderEventPublishMessage.class.getName());
 	public void process(Exchange exchange) throws Exception {
 		JSONObject inBody = new JSONObject(exchange.getIn().getBody(String.class));
+		inBody.put("feeType", "order");
 		exchange.getOut().setBody(inBody);
+		log.debug(inBody);
 		exchange.getOut().setHeader("feeManagement", true);
+		log.debug("header set already");
 	}
 }

aae3c3a54904770ab0559f421ff71c33ac6aa9b3
Arun A <arun@sellinall.com>
2016-07-12 12:49:01 +0530

    updated

--- src/main/java/com/sellinall/order/bl/ConstructOrderNotification.java
+++ src/main/java/com/sellinall/order/bl/ConstructOrderNotification.java
@@ -11,9 +11,9 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.util.JSON;
 import com.mudra.sellinall.config.Config;
 
-public class constructOrderNotification implements Processor {
+public class ConstructOrderNotification implements Processor {
 
-	static Logger log = Logger.getLogger(constructOrderNotification.class.getName());
+	static Logger log = Logger.getLogger(ConstructOrderNotification.class.getName());
 
 	public void process(Exchange exchange) throws Exception {
 		JSONObject inBody = new JSONObject(exchange.getIn().getBody(String.class));
@@ -21,23 +21,21 @@ public class constructOrderNotification implements Processor {
 		JSONObject itemAmount = new JSONObject();
 		JSONObject messageData = new JSONObject();
 		String itemTitle = "";
-		String orderPageUrl=Config.getConfig().getSIAOrderPageURL()+inBody.get("orderID")+"&site="+inBody.get("nickNameID");
+		String orderPageUrl = Config.getConfig().getSIAOrderPageURL() + inBody.get("orderID") + "&site="
+				+ inBody.get("nickNameID");
 		messageData.put("siteNickname", inBody.get("nickNameID"));
 		org.json.JSONArray orderDetails = inBody.getJSONArray("orderItems");
 		for (int i = 0; i < orderDetails.length(); i++) {
 			itemTitle += ((JSONObject) orderDetails.get(i)).get("itemTitle")
-					+ ((orderDetails.length() == (i + 1)) ? "" : ",");			
+					+ ((orderDetails.length() == (i + 1)) ? "" : ",");
 		}
 		messageData.put("itemTitle", itemTitle);
-		if(((JSONObject) inBody.get("buyerDetails")).has("buyerID"))
-		{
-		messageData.put("buyerId", ((JSONObject) inBody.get("buyerDetails")).get("buyerID"));
+		if (((JSONObject) inBody.get("buyerDetails")).has("buyerID")) {
+			messageData.put("buyerId", ((JSONObject) inBody.get("buyerDetails")).get("buyerID"));
+		} else {
+			messageData.put("buyerId", "-");
 		}
-		else
-		{
-			messageData.put("buyerId","-");
-		}
-		messageData.put("orderPageUrl",orderPageUrl);
+		messageData.put("orderPageUrl", orderPageUrl);
 		itemAmount = (JSONObject) inBody.get("orderAmount");
 		messageData.put("itemAmount", itemAmount);
 		messageData.put("orderId", inBody.get("orderID"));

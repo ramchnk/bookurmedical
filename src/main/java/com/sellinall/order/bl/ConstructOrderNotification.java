43ca90bac11a22b24efe87724aea4dfe4b08f2f6
Arun A <arun@sellinall.com>
2016-07-14 15:21:41 +0530

    Email/Sms Notification while order palced

--- src/main/java/com/sellinall/order/bl/ConstructOrderNotification.java
+++ src/main/java/com/sellinall/order/bl/ConstructOrderNotification.java
@@ -3,8 +3,10 @@ package com.sellinall.order.bl;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.log4j.Logger;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import com.mongodb.BasicDBObject;
 import com.mudra.sellinall.config.Config;
 
 public class ConstructOrderNotification implements Processor {
@@ -15,19 +17,25 @@ public class ConstructOrderNotification implements Processor {
 
 		JSONObject inBody = new JSONObject(exchange.getIn().getBody(String.class));
 		JSONObject outBody = new JSONObject();
+
 		try {
 			JSONObject message = new JSONObject();
-			String itemTitle = "";
-			JSONObject site = inBody.getJSONObject("site");
+			BasicDBObject userSiteObject = exchange.getProperty("userSiteSpecificObject", BasicDBObject.class);
+			BasicDBObject nickName = (BasicDBObject) userSiteObject.get("nickName");
+			String nickNameID = nickName.getString("id");
 			String orderPageUrl = Config.getConfig().getSIAOrderPageURL() + inBody.get("orderID") + "&site="
-					+ site.get("nickNameID");
-			message.put("siteNickname", site.get("nickNameID"));
-			org.json.JSONArray orderDetails = inBody.getJSONArray("orderItems");
-			for (int i = 0; i < orderDetails.length(); i++) {
-				itemTitle += ((JSONObject) orderDetails.get(i)).get("itemTitle")
-						+ ((orderDetails.length() == (i + 1)) ? "" : ",");
+					+ nickNameID;
+			String siteNickname = nickNameID.split("-")[0] + "-" + nickName.getString("value");
+			message.put("siteNickname", siteNickname);
+			JSONArray orderItems = inBody.getJSONArray("orderItems");
+			JSONArray items = new JSONArray();
+			for (int i = 0; i < orderItems.length(); i++) {
+				JSONObject itemDetails = new JSONObject();
+				itemDetails.put("title", ((JSONObject) orderItems.get(i)).get("itemTitle"));
+				itemDetails.put("quantity", ((JSONObject) orderItems.get(i)).get("quantity"));
+				items.put(itemDetails);
 			}
-			message.put("itemTitle", itemTitle);
+			message.put("items", items);
 			JSONObject buyerDetails = inBody.getJSONObject("buyerDetails");
 			if (buyerDetails.has("buyerID")) {
 				message.put("buyerId", buyerDetails.getString("buyerID"));
@@ -35,16 +43,17 @@ public class ConstructOrderNotification implements Processor {
 				message.put("buyerId", "-");
 			}
 			message.put("orderPageUrl", orderPageUrl);
-			message.put("itemAmount", inBody.getJSONObject("orderAmount"));
+			message.put("orderAmount", inBody.getJSONObject("orderAmount"));
 			message.put("orderId", inBody.get("orderID"));
 			message.put("orderNumber", inBody.get("orderID"));
 			outBody.put("accountNumber", inBody.get("userId"));
-			outBody.put("userMessageName",(String) exchange.getIn().getHeader("userMessageName"));
+			outBody.put("userMessageName", (String) exchange.getIn().getHeader("userMessageName"));
 			outBody.put("message", message);
 		} catch (Exception exception) {
 			log.error("Error occured while constructing the email-notification payload");
 			exception.printStackTrace();
 		}
+
 		exchange.getOut().setBody(outBody);
 	}
 }

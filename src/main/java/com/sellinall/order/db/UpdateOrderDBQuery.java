79e8b61b6e4c2abbcb3265e4d3e113d7163551ab
NaveenVenkatraj <naveen@sellinall.com>
2016-07-22 10:15:55 +0530

    Minor change

--- src/main/java/com/sellinall/order/db/UpdateOrderDBQuery.java
+++ src/main/java/com/sellinall/order/db/UpdateOrderDBQuery.java
@@ -55,7 +55,7 @@ public class UpdateOrderDBQuery implements Processor {
 		orderRecord.put("orderID", orderMessage.getString("orderID"));
 		String invoiceProfileName = exchange.getProperty("invoiceProfile", String.class);
 		String merchantID = exchange.getProperty("merchantID", String.class);
-		String invoiceSeq = InvoiceSequence.getInvoiceSequence(merchantID, invoiceProfileName);
+		String invoiceNo = InvoiceSequence.getInvoiceSequence(merchantID, invoiceProfileName);
 		//TODO: remove the condition after all publishers start publishing user id.
 		if (orderMessage.containsField("userId")) {
 			orderRecord.put("userId", orderMessage.getString("userId"));
@@ -67,7 +67,7 @@ public class UpdateOrderDBQuery implements Processor {
 		notificationIDList.add(orderMessage.getString("notificationID"));
 		orderRecord.put("notificationID", notificationIDList);
 		orderRecord.put("timeCreated", DateUtil.getSIADateFormat());
-		orderRecord.put("invoiceSeq", invoiceSeq);
+		orderRecord.put("invoiceNo", invoiceNo);
 		fillAdditionDetails(exchange, orderRecord, siteName);
 		DBCollection table = DbUtilities.getInventoryDBCollection("order");
 		table.insert(orderRecord);

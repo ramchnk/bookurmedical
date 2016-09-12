090073495433a8041ccf8f281e9ea6fed0688cc9
raguvarans <raguvaran@sellinall.com>
2016-09-12 17:29:28 +0530

    Updated changes for orderamount not found

--- src/main/java/com/sellinall/order/bl/ProcessOrderStatus.java
+++ src/main/java/com/sellinall/order/bl/ProcessOrderStatus.java
@@ -24,7 +24,6 @@ public class ProcessOrderStatus implements Processor {
 			log.warn("Notification Order Status : " + notificationOrderStatus);
 			throw new Exception("Unknown Notification Order Status"); 
 		}
-		exchange.setProperty("notificationOrderStatus", notificationOrderStatus);
 		notificationOrderActionStatus = NotificationOrderActionStatus.valueOf(orderMessage.getString("orderStatus"));
 		Boolean hasOrderInDB = (Boolean) exchange.getProperty("hasOrderInDB");
 		if (hasOrderInDB) {

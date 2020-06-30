ac4e5889526bd009c70cd6f5f1782152686d88b2
santhoshkmr94 <santhoshkumar@sellinall.com>
2020-06-30 16:15:45 +0530

    partner server db hosting changes

--- src/main/java/com/sellinall/database/InventoryCfg.java
+++ src/main/java/com/sellinall/database/InventoryCfg.java
@@ -1,39 +1,20 @@
 package com.sellinall.database;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.bson.Document;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
 import com.mongodb.MongoClient;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
-import com.mongodb.client.MongoCollection;
+import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.FindOneAndUpdateOptions;
 import com.mudra.sellinall.config.Config;
 
 @Configuration
 public class InventoryCfg {
 
 	public @Bean MongoDatabase db() throws Exception {
-		List<ServerAddress> seeds = new ArrayList<ServerAddress>();
-		String[] hostNames = Config.getConfig().getInventoryCollectionHostName().split(",");
-		String[] ports = Config.getConfig().getInventoryCollectionPort().split(",");
-		for (int i = 0; i < hostNames.length; i++) {
-			seeds.add(new ServerAddress(hostNames[i], Integer.parseInt(ports[i])));
-		}
-
-		List<MongoCredential> credentials = new ArrayList<MongoCredential>();
-		credentials.add(MongoCredential.createScramSha1Credential(Config.getConfig().getDbUserName(),
-				Config.getConfig().getInventoryCollectionDBName(), Config.getConfig().getDbPassword().toCharArray()));
-
-		MongoClient mongoClient = new MongoClient(seeds, credentials);
-		MongoDatabase db = mongoClient.getDatabase(Config.getConfig().getInventoryCollectionDBName());
+		MongoClientURI uri = new MongoClientURI(Config.getConfig().getInventoryConfigDBURI());
+		MongoClient mongoClient = new MongoClient(uri);
+		MongoDatabase db = mongoClient.getDatabase(Config.getConfig().getInventoryConfigDBName());
 		return db;
-	}
+}
 }

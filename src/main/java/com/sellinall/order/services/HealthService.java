b68936b9ff66d61030ec1427fe657df5266bdeb2
revanth-sellinall <revanth@sellinall.com>
2022-05-30 11:50:04 +0530

    test method for stage test

--- src/main/java/com/sellinall/order/services/HealthService.java
+++ src/main/java/com/sellinall/order/services/HealthService.java
@@ -3,6 +3,7 @@ package com.sellinall.order.services;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -10,6 +11,7 @@ import org.apache.http.HttpStatus;
 import org.bson.Document;
 import org.codehaus.jettison.json.JSONException;
 
+import com.mongodb.MongoConfigurationException;
 import com.mongodb.client.MongoCollection;
 import com.sellinall.database.DbUtilities;
 
@@ -17,17 +19,19 @@ import com.sellinall.database.DbUtilities;
 @Produces(MediaType.APPLICATION_JSON)
 public class HealthService {
 	@GET
-	public Response getResponseCode() throws JSONException {
-		if (checkDBHealth()) {
+	public Response getResponseCode(@QueryParam("isMongoException") boolean isMongoException,
+			@QueryParam("isNoClassDefError") boolean isNoClassDefError) throws JSONException {
+		if (checkDBHealth(isMongoException, isNoClassDefError)) {
 			return Response.status(Response.Status.OK).build();
 		}
 		return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
 	}
 
-	private boolean checkDBHealth() {
+	private boolean checkDBHealth(boolean isMongoException, boolean isNoClassDefError) {
 		try {
-			MongoCollection<Document> table = DbUtilities.getDBCollection("accounts");
-			table.countDocuments();
+//			MongoCollection<Document> table = DbUtilities.getDBCollection("accounts");
+//			table.countDocuments();
+			stageTestMethod(isMongoException, isNoClassDefError);
 		} catch (Throwable t) {
 			t.printStackTrace();
 			String error = t.getMessage();
@@ -41,5 +45,14 @@ public class HealthService {
 			}
 		}
 		return true;
+	}
+
+	private void stageTestMethod(boolean isMongoException, boolean isNoClassDefError) {
+		if (isMongoException) {
+			throw new MongoConfigurationException("Unable to look up TXT record for host stage.q5phi.gcp.mongodb.net");
+		} else if (isNoClassDefError) {
+			throw new NoClassDefFoundError("Could not initialize class com.sellinall.database.DbUtilities");
+		}
+
 	}
 }
